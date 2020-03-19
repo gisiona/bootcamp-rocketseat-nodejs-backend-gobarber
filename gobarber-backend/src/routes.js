f7c4b0d9fedeeaa80693c045c8ec04bbfbef4e1d
gisiona <gil-real@hotmail.com>
2020-03-18 23:13:44 -0300

    adicionado e configurado o nodemon e sucrase

--- gobarber-backend/src/routes.js
+++ gobarber-backend/src/routes.js
@@ -1,4 +1,4 @@
-const { Router } = require('express');
+import { Router } from 'express';
 
 const routes = new Router();
 
@@ -7,4 +7,5 @@ routes.get('/', (req, res) => {
     return res.json({ mensagem: "Hello World." })
 });
 
-module.exports = routes;
+//module.exports = routes;
+export default routes;
